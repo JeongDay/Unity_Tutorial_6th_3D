e275a8f0f76f78a3044e2b3cad8aefadd0520a18
JeongDay <cidd1332@gmail.com>
2026-01-15 15:55:38 +0900

    2026.01.15 목요일 - 오후시간 2 인벤토리와 창고 기능

--- Assets/_Farm/02. Scripts/Inventory/Slot.cs
+++ Assets/_Farm/02. Scripts/Inventory/Slot.cs
@@ -1,12 +1,17 @@
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IBeginDragHandler, IDragHandler, IDropHandler, IEndDragHandler
 {
     private IItem item;
 
     [SerializeField] private Button slotButton;
     [SerializeField] private Image slotImage;
+
+    [SerializeField] private Image dragItem;
+    private static Slot dragSlot;
 
     public bool IsEmpty { get; private set; } = true;
 
@@ -42,5 +47,61 @@ public class Slot : MonoBehaviour
         slotImage.gameObject.SetActive(false);
         slotImage.sprite = null;
         slotButton.interactable = false;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (IsEmpty)
+            return;
+
+        dragSlot = this;
+        dragItem.sprite = item.Icon;
+        dragItem.gameObject.SetActive(true);
+        dragItem.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragItem.transform.position = eventData.position; // 마우스 위치로 드래그 아이템 이동
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // 아이템 정보 Swap
+        IItem tempItem = this.item;
+        SetItem(dragSlot.item);
+        dragSlot.SetItem(tempItem);
+
+        Debug.Log("아이템 이동 완료");
+
+        dragItem.sprite = null;
+        dragItem.gameObject.SetActive(false);
+        dragSlot = null;
+    }
+    
+    public void SetItem(IItem newItem)
+    {
+        this.item = newItem;
+        if (newItem == null)
+        {
+            IsEmpty = true;
+            slotImage.sprite = null;
+            slotImage.gameObject.SetActive(false);
+            slotButton.interactable = false;
+        }
+        else
+        {
+            IsEmpty = false;
+            slotImage.sprite = newItem.Icon;
+            slotImage.gameObject.SetActive(true);
+            slotButton.interactable = true;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        dragItem.sprite = null;
+        dragItem.gameObject.SetActive(false);
+        dragSlot = null;
     }
 }
