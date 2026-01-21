37e37a6a109a0daa4838c35907c3f6d3fbb9126e
JeongDay <cidd1332@gmail.com>
2026-01-21 12:06:23 +0900

    Key removed

--- Assets/Async Programming/02. Scripts/StudyLinq.cs
+++ Assets/Async Programming/02. Scripts/StudyLinq.cs
@@ -4,48 +4,75 @@ using System.Linq;
 using UnityEngine;
 
 [Serializable]
-public class Person
+public class Student
 {
-    public string name;
+    public int studentID;
+    public string studentName;
+
+    public Student(int studentID, string studentName)
+    {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+}
+
+[Serializable]
+public class Grade
+{
+    public int studentID;
+    public string subject;
     public int score;
 
-    public Person(string name, int score)
+    public Grade(int studentID, string subject, int score)
     {
-        this.name = name;
+        this.studentID = studentID;
+        this.subject = subject;
         this.score = score;
     }
 }
 
 public class StudyLinq : MonoBehaviour
 {
-    public List<Person> persons = new List<Person>();
-    
-    public int cutline = 70;
+    public List<Student> students = new List<Student>();
+    public List<Grade> grades = new List<Grade>();
+
+    void Awake()
+    {
+        students.Add(new Student(1, "Alice"));
+        students.Add(new Student(2, "Bob"));
+        students.Add(new Student(3, "Charlie"));
+        students.Add(new Student(4, "Eve"));
+
+        grades.Add(new Grade(1, "Math", 90));
+        grades.Add(new Grade(2, "Science", 85));
+        // grades.Add(new Grade(3, "English", 92));
+        // grades.Add(new Grade(4, "Math", 78));
+    }
 
     void Start()
     {
-        persons.Add(new Person("John", 65));
-        persons.Add(new Person("Sarah", 80));
-        persons.Add(new Person("David", 95));
-        persons.Add(new Person("Emily", 70));
-        persons.Add(new Person("Michael", 50));
+        // var innerJoin = from student in students
+        //                 join grade in grades on student.studentID equals grade.studentID
+        //                 select new
+        //                 {
+        //                     StudentID = student.studentID,
+        //                     StudentName = student.studentName,
+        //                     Subject = grade.subject,
+        //                     Score = grade.score
+        //                 };
 
-        CheckScore();
-    }
+        var outerJoin = from student in students
+                        join grade in grades on student.studentID equals grade.studentID into studentGrades
+                        from studentGrade in studentGrades.DefaultIfEmpty()
+                        select new
+                        {
+                            StudentID = student.studentID,
+                            StudentName = student.studentName,
+                            Subject = studentGrade?.subject ?? "None", // subject가 null이면 null로 적용
+                            Score = studentGrade?.score ?? 0 // score가 null이면 0으로 적용
+                        };
 
-    private void CheckScore()
-    {
-        // var passPersons = from person in persons
-        //                 where person.score >= cutline
-        //                 select person;
-
-        var passPersons = persons.Where(p => p.score >= cutline);
-        var failPersons = persons.Except(passPersons);
-        
-        foreach (var p in passPersons)
-            Debug.Log($"<color=green>{p.name}</color>");
-        
-        foreach (var p in failPersons)
-            Debug.Log($"<color=red>{p.name}</color>");
+        foreach (var item in outerJoin)
+            Debug.Log($"{item.StudentID}, {item.StudentName}, {item.Subject}, {item.Score}");
     }
 }
